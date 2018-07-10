2bb7462c34a10e1585063412273399f6e48d1e0c
Leon Feng <best.sum@gmail.com>
2018-07-10 18:11:27 -0400

    add rabittmq

--- worker/worker.py
+++ worker/worker.py
@@ -1,8 +1,8 @@
 from celery import Celery
 import time
 
-CELERY_BROKER_URL = "redis://localhost:6379/0"
-CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
+CELERY_BROKER_URL = 'amqp://myuser:mypassword@localhost:5672/myvhost'
+CELERY_RESULT_BACKEND = 'amqp://myuser:mypassword@localhost:5672/myvhost'
 
 app = Celery('worker', broker=CELERY_BROKER_URL, backend=CELERY_RESULT_BACKEND)
 
